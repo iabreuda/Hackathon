9f3fc5a5d6f5b55f8961b56751c8cef0d2a000c2
Varlen Neto <varlenneto@poli.ufrj.br>
2016-05-17 23:18:31 -0300

    Adds Edit Profile page

--- www/js/views/ProfileView.js
+++ www/js/views/ProfileView.js
@@ -14,6 +14,9 @@ var ProfileView = function (service) {
           case "mensagens":
             var obj = {context: this.template_mensagens, mensagens:true }
             break;
+          case "edit-profile":
+            var obj = {context: this.template_edit, editprofile: true }
+            break;
         }
         this.$el.html(this.template(obj));
         return this;
