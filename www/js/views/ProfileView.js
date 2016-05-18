a4659cbef5e6e4d2abf880fa354416bbbb2257ba
Varlen Neto <varlenneto@poli.ufrj.br>
2016-05-17 22:54:26 -0300

    Adds selected page button highlight on profile page

--- www/js/views/ProfileView.js
+++ www/js/views/ProfileView.js
@@ -3,16 +3,16 @@ var ProfileView = function (service) {
     this.render = function renderProfileView(name) {
         switch(name) {
           case "dashboard":
-            var obj = {context: this.template_dashboard}
+            var obj = {context: this.template_dashboard, dashboard: true }
             break;
           case "my-projects":
-            var obj = {context: this.template_myprojects}
+            var obj = {context: this.template_myprojects, myprojects: true }
             break;
           case "inscricoes":
-            var obj = {context: this.template_inscricoes}
+            var obj = {context: this.template_inscricoes, inscricoes: true }
             break;
           case "mensagens":
-            var obj = {context: this.template_mensagens}
+            var obj = {context: this.template_mensagens, mensagens:true }
             break;
         }
         this.$el.html(this.template(obj));
