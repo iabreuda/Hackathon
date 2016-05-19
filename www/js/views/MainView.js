95708767fd3a6aafb017a033405f8d8ceb596207
Igor Abreu <igorabreu.ds@poli.ufrj.br>
2016-05-19 10:13:25 -0300

    Retrieve projects from backendless to show in main page

--- www/js/views/MainView.js
+++ www/js/views/MainView.js
@@ -1,16 +1,32 @@
 var MainView = function (service) {
 
-    this.render = function renderMainView() {
-        var obj = {title: "Hackathon 2016", description: "AAAAAA" };
-        this.$el.html(this.template(obj));
-        return this;
+    var self = this;
+
+    this.data = {
+        Nome: "Exemplo",
+        Descricao: "Descrição do projeto - Lorem ipsum dolor sit amet, consectetur adipisici elit,sed eiusmod tempor incidunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisici elit,sed eiusmod tempor incidunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisici elit,sed eiusmod tempor incidunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisici elit,sed eiusmod tempor incidunt ut labore et dolore magna aliqua.",
+        Interesses: "Áreas de interesse do projeto",
+        Habilidades: "Habilidades relacionadas ou requisitadas pelo projeto"
+    };
+
+    this.render = function renderMainView(data) {
+      records = this.data.data;
+      projects = [];
+      for (var i = 0; i < 3; i++) {
+        var rand = records[Math.floor(Math.random() * records.length)];
+        projects[i] = {id: rand.objectId, title: rand.Nome, description: rand.Descricao };
+      };
+
+      this.$el.html(this.template({"projects": projects}));
+      return this;
     };
 
     this.initialize = function() {
        // Define a div wrapper for the view ( used to attach events )
        this.$el = $('<div/>');
-       this.render();
+       this.data = service.retrieveAllProject();
     }
+
 
     this.initialize();
 }
